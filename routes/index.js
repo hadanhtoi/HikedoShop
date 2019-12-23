1df501df25632923e0313d1d7c1734b41cc25574
aquarius <toihd@bitnemo.vn>
2019-12-23 11:29:49 +0700

    homPage-slideShow

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { page: 'Hikedo Shop - Shoes Online Shop' });
+router.get('/', function (req, res, next) {
+  res.render('index', { page: 'Sunny Shop - Shoes Online Shop' });
 });
 
 module.exports = router;
