7c7998dc343fc15fa186f6259798bdd67d2c7ca6
aquarius <toihd@bitnemo.vn>
2020-01-04 18:55:30 +0700

    add user profile, user change profile process, user change password

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 /* GET home page. */
-router.get('/', forwardAuthenticated, function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { page: 'Sunny Shop - Shoes Online Shop' });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const moment = require('moment');
+const flash = require('connect-flash');
 const phone_regex = require('../config/constant').PHONE_REGEX;
 const birthday_regex = require('../config/constant').BIRTHDAY_REG;
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
@@ -14,7 +15,7 @@ router.get('/signIn', forwardAuthenticated, (req, res, next) => {
   res.render('signIn', { page: "Sign In Page" });
 });
 
-router.get('/signUp', forwardAuthenticated, (req, res, next) => {
+router.get('/signUp', (req, res, next) => {
   res.render('signUp', { page: "Sign Up Page" });
 });
 // Profile
@@ -23,75 +24,153 @@ router.get('/profile', ensureAuthenticated, (req, res) => {
   res.render('userProfile', {
     page: "User Profile",
     user: req.user,
-    createdAt: createdAt
+    createdAt: createdAt,
   });
 });
-// Edit profile: open router profile
 
-  /**console.log(req.user);
-   * {
-  email: 'xunglenhho@gmail.com',
-  birthday: '12/12/1212',
-  createdAt: 2019-12-26T11:19:34.957Z,
-  image: '/images/man-user.png',
-  _id: 5e04974623a6a51af0aacdf1,
-  name: 'Lệnh Hồ Xung',
-  password: '$2a$10$k0Q9FXxDr1ZhFWtFsj9NFugldaFSlrk93Mym1HaJj02FselgA1WAC',
-  phone: '0353221074',
-  __v: 0
-}
-  */
- router.get('/editProfile', ensureAuthenticated, function (req, res, next) {
-  const { name, email } = req.user;
-  const errors=[];
-  console.log(req.body)
-  errors.push({msg:"Nếu email để trống, sau khi cập nhật sẽ được cập nhật thành rỗng!"});
-  errors.push({msg:"Các trường khác bắt buộc không để trống nếu bạn muốn cập nhật!"});
-  errors.push({msg:"Password và Confirm Password "})
-  res.render("editProfile", {
-    page: "Edit Profile",
-    name,
-    email,
-    user: req.user,
-    errors,
+router.post('/profile/editUserProfile', ensureAuthenticated, (req, res, next) => {
+  const { name, email, address, confirmPassword } = req.body;
+  let errors = [];
+  // if(!name){
+  //   errors.push({msg:"Tên không được để trống!"});
+  // }
+  // if(!email){
+  //   errors.push({msg:"Email không được để trống!"});
+  // }
+  // if(!address ||address == "" || address == null || address == 'undefined'){
+  //   errors.push({msg:"Địa chỉ không được để trống!"});
+  // }
+  // if(!confirmPassword){
+  //   errors.push({msg:"Confirm Password không được để trống!"});
+  // }
+  // if(errors.length > 0){
+  //   errors.push({msg:"Nhấn Edit nếu muốn tiếp tục Edit,"});
+  //   res.render('userProfile',{
+  //     page:"User Profile",
+  //     user:req.user,
+  //     errors,
+  //     createdAt:moment(req.user.createdAt).format('MM/DD/YYYY')
+  //   });
+  if (!name || !address || !confirmPassword) {
+    errors.push("Trừ email, không thể update dữ liệu trống!");
+  }
+  if(confirmPassword && confirmPassword.length < 6){
+    errors.push("Password phải từ 6 kí tự trở lên!");
+  }
+  if (errors.length > 0) {
+    req.flash("error_msg",errors)    
+    res.redirect('/users/profile');
+  } else {
+    //Compare confirm password
+    bcrypt.compare(confirmPassword, req.user.password , (err, result) => {
+      if (result != true) {
+        // errors.push({ msg: "Confirm Password không đúng!" });
+        // res.render("userProfile", {
+        //   page: "User Profile",
+        //   errors,
+        //   user: req.user,
+        //   createdAt: moment(req.user.createdAt).format('MM/DD/YYYY')
+        // });
+        req.flash("error_msg", "Confirm Password không đúng!");
+        res.redirect('/users/profile');
+      } else {
+        User.findOne({ _id: req.user.id, password: req.user.password }).then((user) => {
+          if (!user) {
+            //Not found user
+            errors.push({ msg: "Không tìm thấy tài khoản đã đăng nhập!" });
+            req.logout();
+            res.render("signIn", {
+              page: "User Profile",
+              errors,
+            });
+          } else {
+            //Found user
+            if (user.name !== name) {
+              user.name = name;
+            }
+            if (user.email !== email) {
+              user.email = email;
+            }
+            if (user.address == null || user.address == 'undefined' || user.address == "" || user.address != address) {
+              user.address = address;
+            }
+            user.save()
+              .then(user => {
+                req.flash("success_msg", "Bạn đã update thành công!");
+                res.redirect("/users/profile")
+              })
+              .catch(err => console.log("Edit Profile Error: ", err))
+          }
+        });
+      }
+    });
+  }
+});
+
+router.post('/profile/changePassword', ensureAuthenticated, (req, res, next) => {
+  const { oldPassword, newPassword, confirmNewPassword } = req.body;
+  let errors2 = [];
+
+  if (!oldPassword || !newPassword || !confirmNewPassword) {
+    errors2.push({ msg: "Không được bỏ trống trường nào!" });
+  }
+
+  if (newPassword && confirmNewPassword && (confirmNewPassword !== newPassword)) {
+    errors2.push({ msg: "Confirm New Password nhập không đúng!" });
+  }
+  if (errors2.length > 0) {
+    // res.render('userProfile', {
+    //   page: "User Profile",
+    //   errors2,
+    //   user: req.user,
+    //   createdAt: moment(req.user.createdAt).format('MM/DD/YYYY')
+    // });
+    req.flash("errorsArr",errors2);
+    res.redirect('/users/profile');
+  } else {
+    bcrypt.compare(oldPassword, req.user.password,(err,result)=>{
+      if(err){
+        throw err;
+      }
+      if(result == false){      
+      req.flash("warning", "Confirm Password nhập chưa đúng");
+      res.redirect('/users/profile');
+    } else {
+      //Confirm password true
+      User.findById(req.user.id).then(user => {
+        if (!user) {
+          req.flash("warning", "Không tìm thấy user đã đăng nhập!")
+          res.redirect('/users/signIn');
+        } else {
+          //Found user
+          //Hash password
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newPassword, salt, (err, hash) => {
+              if (err) throw err;
+              //Set password to hashed
+              user.password = hash;
+              //Save new user
+              user.save()
+                .then(user => {
+                  req.logout();
+                  req.flash('success_msg', "Bạn đã thay đổi password thành công. Giờ bạn phải đăng nhập lại!");
+                  res.redirect('/users/signIn');
+                })
+                .catch(err => console.log("SignUp Error: ", err));
+            });
+          });
+        }
+      });
+    }
   });
+};
 });
-// Edit profile post submit
-
-  //req.body lưu giá trị của edit user form sau khi cập nhật gửi lên.
-  /**req.user: Lưu thông tin của user từ profile gửi sang
-   * {
-  email: 'xunglenhho@gmail.com',
-  birthday: '12/12/1212',
-  createdAt: 2019-12-26T11:19:34.957Z,
-  image: '/images/man-user.png',
-  _id: 5e04974623a6a51af0aacdf1,
-  name: 'Lệnh Hồ Xung',
-  password: '$2a$10$k0Q9FXxDr1ZhFWtFsj9NFugldaFSlrk93Mym1HaJj02FselgA1WAC',
-  phone: '0353221074',
-  __v: 0
-}*/
-router.post('/editProfile', (req, res, next) => {
-const {name,email,newPassword,newPassword2,} = req.body;
-const {_id} = req.user;
-let errors = [];
-
-if (!name || !newPassword || !newPassword2) {
-  errors.push({ msg: 'Name, New Password, Confirm New Password bắt buộc phải điền!' });
-}
-
-if (email.includes("@") == false) {
-  errors.push({ msg: "Email phải chứa @" });
-}
-  res.send("oki")
-});
-
-router.post('/signUp', function (req, res, next) {
-  const { name, email, password, password2, phone, birthday } = req.body;
+router.post('/signUp', forwardAuthenticated, function (req, res, next) {
+  const { name, email, password, password2, phone, birthday, address } = req.body;
   let errors = [];
 
   //to-do: check required fields
-  if (!name || !password || !password2 || !phone || !birthday) {
+  if (!name || !password || !password2 || !phone || !birthday || !address) {
     errors.push({ msg: 'Trừ Email là không bắt buộc, các trường khác bắt buộc phải điền' });
   }
 
@@ -115,6 +194,10 @@ router.post('/signUp', function (req, res, next) {
     errors.push({ msg: "Password phải từ 6 kí tự trở lên" });
   }
 
+  if (address == "" || address == null || address == 'undefined') {
+    errors.push({ msg: "Address không nên bỏ trống!" });
+  }
+
   if (errors.length > 0) {
     res.render('signUp', {
       page: "Sign Up Page",
@@ -124,14 +207,12 @@ router.post('/signUp', function (req, res, next) {
       password,
       password2,
       phone,
-      birthday
+      birthday,
+      address
     });
-  } else {
+  }else {
     //Validation pass
-    User.findOne({ phone: phone }).then((err, user) => {
-      if (err) {
-        console.log(err);
-      }
+    User.findOne({ phone: phone }).then((user) => {
       if (user) {
         //user exists
         errors.push({ msg: "Số điện thoại đã được sử dụng. Hãy lấy số điện thoại khác" });
@@ -143,7 +224,8 @@ router.post('/signUp', function (req, res, next) {
           password,
           password2,
           phone,
-          birthday
+          birthday,
+          address
         });
       } else {
         //create new user
@@ -153,9 +235,12 @@ router.post('/signUp', function (req, res, next) {
           password,
           phone,
           birthday,
-          createdAt: Date.now()
+          address,
+          createdAt: Date.now(),
+          updatedAt: Date.now(),
+          authenticate: 2
         });
-        console.log(newUser);
+        console.log("New user: \n" + newUser);
 
         //Hash password
         bcrypt.genSalt(10, (err, salt) => {
@@ -169,7 +254,7 @@ router.post('/signUp', function (req, res, next) {
                 req.flash('success_msg', "Bạn đã đăng kí thành công. Giờ bạn có thế sign in");
                 res.redirect('/users/signIn');
               })
-              .catch(err => console.log(err));
+              .catch(err => console.log("SignUp Error: ", err));
           });
         });
       }
