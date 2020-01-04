35495a8e09336f16910b67800e279b20271c309c
aquarius <toihd@bitnemo.vn>
2020-01-04 18:52:58 +0700

    add user fields

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
@@ -22,16 +23,39 @@ const UserSchema = mongoose.Schema({
     require: true,
     default: ""
   },
+  address: {
+    type: String,
+    require: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now()
   },
-  image:{
-    type:String,
-    require:false,
-    default:'/images/man-user.png'
-  }  
+  updatedAt: {
+    type: Date,
+    default: Date.now()
+  },
+  authentication: {
+    type: Number,
+    require: false,
+    default: 2,
+  },
+  image: {
+    type: String,
+    require: false,
+    default: '/images/man-user.png'
+  }
 });
+// methods ======================
+// phương thực sinh chuỗi hash
+// UserSchema.methods.generateHash = function (password) {
+//   return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+// };
+
+// kiểm tra password có hợp lệ không
+// UserSchema.methods.validPassword = function (password) {
+//   return bcrypt.compareSync(password, this.local.password);
+// };
 
 const User = mongoose.model("User", UserSchema);
 
